e07af87ffa8766781f8aae8a07c3e9d6650587b0
Kevin Gallaccio <gallacciokevin@gmail.com>
2023-10-30 19:32:27 +0100

    V2 Changes

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -3,8 +3,13 @@ package com.coderscampus;
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	//generate a random number
+	
+	// and collect user input
+		
+	// etc...
+		
 	}
 
 }

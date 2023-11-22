8d95a2947e892d2c55d36ffba961c180559907c2
Kevin Gallaccio <gallacciokevin@gmail.com>
2023-11-22 16:39:22 +0100

    Bug fixed (hopefully there won't be more)

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -11,37 +11,41 @@ public class Assignment2Application {
 		int numberToGuess = random.nextInt(1, 101);
 
 		Scanner scanner = new Scanner(System.in);
-		int userInput;
+		int userInput = 0;
 
-		System.out.println("Pick a number between 1 and 100: ");
-		userInput = scanner.nextInt();
+		System.out.println("Pick a number between 1 and 100: (random:" + numberToGuess + ")");
 
 		int numberOfGuesses = 0;
-		int maxNumberOfGuesses = 4;
+		int maxNumberOfGuesses = 5;
 
 		while (numberOfGuesses < maxNumberOfGuesses) {
-			int remainingGuesses = maxNumberOfGuesses - numberOfGuesses;
+			int remainingGuesses = maxNumberOfGuesses - numberOfGuesses - 1;
+
+			userInput = scanner.nextInt();
 
 			if (userInput < 1 || userInput > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
-			} else if (userInput < numberToGuess) {
-				System.out.println("Pick a higher number, you have " + (remainingGuesses) + " guess"
-						+ (remainingGuesses == 1 ? "" : "es") + " left");
-				numberOfGuesses++;
-			} else if (userInput > numberToGuess) {
-				System.out.println("Pick a lower number, you have " + (remainingGuesses) + " guess"
-						+ (remainingGuesses == 1 ? "" : "es") + " left");
-				numberOfGuesses++;
 			} else {
-				System.out.println("You Win!");
-				break;
+				if (remainingGuesses == 0) {
+					break;
+					} if (userInput < numberToGuess) {
+					System.out.println("Pick a higher number, you have " + remainingGuesses + " guess"
+							+ (remainingGuesses == 1 ? "" : "es") + " left");
+				} else if (userInput > numberToGuess) {
+					System.out.println("Pick a lower number, you have " + remainingGuesses + " guess"
+							+ (remainingGuesses == 1 ? "" : "es") + " left");
+				} else {
+					System.out.println("You Win!");
+					break;
+				}
+				numberOfGuesses++;
 			}
-			userInput = scanner.nextInt();
 		}
-		if (userInput != numberToGuess) {
-			System.out.println("You Lose ! the number was " + numberToGuess);
-		}
-		scanner.close();
 
+		if (userInput != numberToGuess) {
+			System.out.println("You Lose! The number was " + numberToGuess);
+		}
+
+		scanner.close();
 	}
 }
